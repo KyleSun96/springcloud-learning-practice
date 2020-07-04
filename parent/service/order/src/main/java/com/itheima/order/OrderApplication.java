5415e95eddcabef683c3f64dea38597b5bbcfcb6
KyleSun <swy0907163@163.com>
2020-07-05 03:32:12 +0800

    :hammer:重构代码

--- parent/service/order/src/main/java/com/itheima/order/OrderApplication.java
+++ parent/service/order/src/main/java/com/itheima/order/OrderApplication.java
@@ -1,8 +1,10 @@
 package com.itheima.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Program: Review03_springcloud
@@ -12,6 +14,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@MapperScan("com.itheima.order.mapper")
 public class OrderApplication {
 
     public static void main(String[] args) {
