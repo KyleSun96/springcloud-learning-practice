60ae5a1eb8948f19ec5f07aab0793cd0961249e0
KyleSun <swy0907163@163.com>
2020-07-04 19:30:07 +0800

    :memo:搭建注册中心

---
+++ parent/eureka/src/main/java/com/itheima/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.itheima.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Program: Review03_springcloud
+ * @ClassName: EurekaApplication
+ * @Description:
+ * @Author: KyleSun
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
