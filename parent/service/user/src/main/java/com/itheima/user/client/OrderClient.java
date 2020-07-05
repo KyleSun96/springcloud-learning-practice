f27fa6486aac365a0f235092c75c49cc1b942d96
KyleSun <swy0907163@163.com>
2020-07-05 21:20:22 +0800

    :bug:修复feign映射模糊异常：不能在类上加@RequestMapping("/order")，易出现方法映射模糊异常。在方法上写("/order")即可。

--- parent/service/user/src/main/java/com/itheima/user/client/OrderClient.java
+++ parent/service/user/src/main/java/com/itheima/user/client/OrderClient.java
@@ -1,21 +1,29 @@
 package com.itheima.user.client;
 
 import com.itheima.entity.Result;
-import com.itheima.user.fallback.OrderFallback;
+import com.itheima.pojo.Order;
+import com.itheima.user.client.fallback.OrderFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @Description:
  * @Author: KyleSun swy0907163@163.com
  **/
 @FeignClient(name = "order-server", fallback = OrderFallback.class)
-@RequestMapping("/order")
 public interface OrderClient {
 
-    @GetMapping("/findOrdersByUsername/{username}")
-    public Result findOrdersByUsername(@PathVariable(value = "username") String username);
+    /**
+     * @description: //TODO 根据⽤户的username查询数据库的订单
+     * @param: [username]
+     * @return: com.itheima.entity.Result
+     * @author: KyleSun swy0907163@163.com
+     */
+    @GetMapping("/order/findOrdersByUsername/{username}")
+    public Result<List<Order>> findOrdersByUsername(@PathVariable(value = "username") String username);
 
 }
