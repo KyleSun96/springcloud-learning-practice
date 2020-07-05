200aadca7fbac906ee8cd8af3be9157d1a2f6afa
KyleSun <swy0907163@163.com>
2020-07-05 10:42:22 +0800

    :sparkles:订单服务注册到注册中心

--- parent/service/order/src/main/java/com/itheima/order/OrderApplication.java
+++ parent/service/order/src/main/java/com/itheima/order/OrderApplication.java
@@ -3,17 +3,14 @@ package com.itheima.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Description:
  * @Author: KyleSun swy0907163@163.com
  **/
 @SpringBootApplication
-@EnableCircuitBreaker // 开启Hystrix功能
-@EnableDiscoveryClient // 包含了开启熔断和@EnableEurekaClient
+@EnableDiscoveryClient // 包含了开启熔断@EnableCircuitBreaker和@EnableEurekaClient
 //@EnableFeignClients(basePackages = "com.itheima.order.feign")
 @MapperScan(basePackages = "com.itheima.order.mapper")
 public class OrderApplication {
